f3f0b6d4fb54a569ca2b49b81daf5a257877cbfd
Abdullah Ibnu Hibban <abd.ibnuhibban@gmail.com>
2021-01-23 10:52:28 +0700

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -464,7 +464,7 @@ use Illuminate\\Database\\Migrations\\Migration;
 use Illuminate\\Database\\Schema\\Blueprint;
 use Illuminate\\Support\\Facades\\Schema;
 
-class Create${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Table extends Migration{
+class Create${table.val().charAt(0).toUpperCase() + table.val().substr(1)}sTable extends Migration{
     
     public function up(){
         Schema::create('${table.val().toLowerCase().replace(" ", "_")}', function (Blueprint $table) {
