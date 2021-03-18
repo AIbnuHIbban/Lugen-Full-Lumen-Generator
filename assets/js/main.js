19da4a3987b49443244be2ffe33839da92c276ac
aibnuhibban <abd.ibnuhibban@gmail.com>
2021-03-18 14:34:16 +0700

    add unit testing needed

--- assets/js/main.js
+++ assets/js/main.js
@@ -249,7 +249,9 @@ $router->group(['prefix'=>'api/v1'], function() use($router){
 });`)
 
         res_artisan.text(`php artisan make:model ${table} -mrc
-php artisan make:transformer ${table}`)
+php artisan make:transformer ${table}
+php artisan make:test ${table}
+`)
     
     cp_transform.text('')
     cp_model.text('')
@@ -272,15 +274,18 @@ function generate(){
     var res_transform   = $('#result_transform')
     var res_model       = $('#result_models')
     var res_crud        = $('#result_crud')
+    var res_testing     = $('#result_testing')
     var res_migration   = $('#result_migration')
     var res_routes      = $('#result_routes')
     var res_artisan     = $('#result_artisan')    
     var cp_transform    = $('#clipboard_transform')
     var cp_model        = $('#clipboard_models')
     var cp_crud         = $('#clipboard_crud')
+    var cp_testing      = $('#clipboard_testing')
     var cp_migration    = $('#clipboard_migration')
     var cp_routes       = $('#clipboard_routes')
     var cp_artisan      = $('#clipboard_artisan')
+
     let kolom           = ""
     if (table.val() === "" || list_column_val === "") {
         Swal.fire(
@@ -334,6 +339,14 @@ function generate(){
                 transform += `'${list_column[i]}'        => (string) $${table.val()}->${list_column[i]},`
             }else{
                 transform += `'${list_column[i]}'        => (string) $${table.val()}->${list_column[i]},\n            `
+            }
+        }
+        let testing               = ""
+        for (let i = 0; i < list_column.length; i++) {
+            if (i === list_column.length-1) {
+                testing += `"${list_column[i]}"         => 'Test Value'`
+            }else{
+                testing += `"${list_column[i]}"         => 'Test Value',\n            `
             }
         }
         res_model.text(`<?php
@@ -412,11 +425,11 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller e
 
         $fractal = fractal($${table.val()}, new ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Transformer())->toArray();
 
-        return response()->json($fractal, Response::HTTP_CREATED);
+        return response()->json($fractal, Response::HTTP_OK);
     }
 
     public function index_id($id){
-        $data = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::findOrFail($id);
+        $${table.val()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($id);
         
         $fractal = fractal($${table.val()}, new ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Transformer())->toArray();
     
@@ -435,15 +448,11 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller e
 
         $fractal = fractal($${table.val()}, new ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Transformer())->toArray();
    
-        return response()->json($fractal, Response::HTTP_OK);
+        return response()->json($fractal, Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id){
-        $this->validate($request, [
-            ${validasi}
-        ]);
-
-        $attrs    = [];
+        $attrs    = $request->all();
         $${table.val()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::findOrFail($id);
         $${table.val()}->fill($attrs);
 
@@ -468,6 +477,88 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller e
      * Thanks For Using Laragen
      */
 }`)
+
+        res_testing.text(`<?php
+class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Test extends TestCase{
+    /**
+     * /${table.val()} [GET]
+     */
+    public function testShouldReturnAll${table.val().charAt(0).toUpperCase() + table.val().substr(1)}(){
+
+        $this->get("v1/${table.val()}", []);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            'data' => ['*' => "id",${kolom} ],
+            'meta' => [
+                '*' => [
+                    'total',
+                    'count',
+                    'per_page',
+                    'current_page',
+                    'total_pages',
+                    'links',
+                ]
+            ]
+        ]);
+        
+    }
+
+    /**
+     * /${table.val()}/id [GET]
+     */
+    public function testShouldReturn${table.val().charAt(0).toUpperCase() + table.val().substr(1)}(){
+        $this->get("v1/${table.val()}/1", []);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['data' => "id",${kolom} ]
+        );
+        
+    }
+
+    /**
+     * /${table.val()} [POST]
+     */
+    public function testShouldCreate${table.val().charAt(0).toUpperCase() + table.val().substr(1)}(){
+
+        $parameters = [ 
+            ${testing} 
+        ];
+
+        $this->post("v1/${table.val()}", $parameters, []);
+        $this->seeStatusCode(201);
+        $this->seeJsonStructure(
+            ['data' => "id",${kolom} ]
+        );
+        
+    }
+    
+    /**
+     * /${table.val()}/id [PUT]
+     */
+    public function testShouldUpdate${table.val().charAt(0).toUpperCase() + table.val().substr(1)}(){
+
+        $parameters = [ 
+            ${testing} 
+        ];
+
+        $this->put("v1/${table.val()}/1", $parameters, []);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure(
+            ['data' => "id",${kolom} ]    
+        );
+    }
+
+    /**
+     * /${table.val()}/id [DELETE]
+     */
+    public function testShouldDelete${table.val().charAt(0).toUpperCase() + table.val().substr(1)}(){
+        
+        $this->delete("v1/${table.val()}/1", [], []);
+        $this->seeStatusCode(200);
+    }
+
+}`)
+
         res_migration.text(`<?php
     
 use Illuminate\\Database\\Migrations\\Migration;
@@ -512,6 +603,7 @@ $router->group(['prefix'=>'api/v1'], function() use($router){
 
         res_artisan.text(`php artisan make:model ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} -mrc
 php artisan make:transformer ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Transformer
+php artisan make:test ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Test 
 
 `)
 
